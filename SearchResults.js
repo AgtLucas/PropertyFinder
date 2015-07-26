4a69538ef55c146070df2387e3a3d4cd0b828929
Lucas <lucas@agtlucas.com>
2015-07-25 22:39:03 -0300

    Update SearchResults

--- SearchResults.js
+++ SearchResults.js
@@ -11,6 +11,34 @@ var {
   Text
 } = React;
 
+var styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+  textContainer: {
+    flex: 1
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  }
+});
+
 class SearchResults extends React.Component {
 
   constructor(props) {
@@ -24,10 +52,21 @@ class SearchResults extends React.Component {
   }
 
   renderRow(rowData, sectionID, rowID) {
+    var price = rowData.price_formatted.split(' ')[0];
+
     return(
-      <TouchableHighlight underlayColor='#dddddd'>
+      <TouchableHighlight
+        onPress={() => this.rowPressed(rowData.guid)}
+        underlayColor='#dddddd'>
         <View>
-          <Text>{rowData.title}</Text>
+          <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{ uri: rowData.img_url }} />
+            <View style={styles.textContainer}>
+              <Text style={styles.price}>£{price}</Text>
+            <Text style={styles.title} numberOfLines={1}>{rowData.title}</Text>
+            </View>
+          </View>
+          <View style={styles.separator} />
         </View>
       </TouchableHighlight>
     );
